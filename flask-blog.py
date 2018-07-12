b07877b7fd80e836f353f1bee1f6f79a3279884d
Evan Salzbrenner <hello@aboutevan.com>
2018-07-11 20:42:46 -0700

    templates

--- flask-blog.py
+++ flask-blog.py
@@ -1,14 +1,34 @@
-from flask import Flask
+# export FLASK_APP=flask-blog.py
+# export FLASK_DEBUG=1
+# flask run
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Evan S',
+        'title': 'Blog Post 1',
+        'content': 'First Post',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second Post',
+        'date_posted': 'April 20, 2018'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello Woffrld!</h1>"
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
